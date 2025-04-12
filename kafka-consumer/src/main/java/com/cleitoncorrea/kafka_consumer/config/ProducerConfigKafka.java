3cde88969dbdb0f8bf0d97af2aeb7929d8b0dc1d
CleitonCorrea <cleitoncorrea2013@hotmail.com>
2025-04-11 23:00:59 -0300

    create producer services kafka

--- kafka-consumer/src/main/java/com/cleitoncorrea/kafka_consumer/config/ProducerConfigKafka.java
+++ kafka-consumer/src/main/java/com/cleitoncorrea/kafka_consumer/config/ProducerConfigKafka.java
@@ -3,8 +3,10 @@ package com.cleitoncorrea.kafka_consumer.config;
 import com.cleitoncorrea.kafka_consumer.model.TransactionMessage;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.UUIDSerializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
@@ -14,11 +16,17 @@ import java.util.UUID;
 
 @Configuration
 public class ProducerConfigKafka {
+    @Bean
     public ProducerFactory< UUID, TransactionMessage > producerFactory(){
         Map<String, Object> configProps = new HashMap <> ();
         configProps.put ( ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092" );
         configProps.put ( ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, UUIDSerializer.class );
         configProps.put ( ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class );
         return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    KafkaTemplate<UUID, TransactionMessage> KafkaTemplate(){
+        return new KafkaTemplate <> ( producerFactory () );
     }
 }
